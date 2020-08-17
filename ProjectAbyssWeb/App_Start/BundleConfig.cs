293818ac7ce66edb5d96d204fbc5a20c9b6dffd6
Steven Grey <stevengrey@hotmail.fr>
2020-08-17 16:02:30 +0200

    Ajout d'un fichier CSS + Connexion entre les pages + Début de la construction de la page Options

--- ProjectAbyssWeb/App_Start/BundleConfig.cs
+++ ProjectAbyssWeb/App_Start/BundleConfig.cs
@@ -5,7 +5,7 @@ namespace ProjectAbyssWeb
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        // Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,8 +14,8 @@ namespace ProjectAbyssWeb
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
+            // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
---
+++ ProjectAbyssWeb/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectAbyssWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ConfigPlayers()
+        {
+            return View();
+        }
+
+        public ActionResult AddElement()
+        {
+            return View();
+        }
+
+        public ActionResult DisplaySetup()
+        {
+            return View("Setup");
+        }
+    }
+}
